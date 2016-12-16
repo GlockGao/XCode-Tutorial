d2f8b7050755130ea7f2855f144f622a6c3640fd
GlockGao <GlockGao@users.noreply.github.com>
2016-12-16 15:14:26 +0800

    Update ViewController.swift

--- RemoteGitTest/ViewController.swift
+++ RemoteGitTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Are you sure?
     }
 
     override func didReceiveMemoryWarning() {
